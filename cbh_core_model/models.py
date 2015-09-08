dc9b90dae2c4950050cb69cdc79edf1f52315255
paulbarrett79 <paulbarrett79@gmail.com>
2015-09-08 14:48:13 +0100

    data for multiselect

--- cbh_core_model/models.py
+++ cbh_core_model/models.py
@@ -351,6 +351,12 @@ class PinnedCustomField(TimeStampedModel):
         searchdata = [{"label" : "[%s] %s" % (self.name ,item.strip()), "value" : "%s|%s" % (self.name ,item.strip())} for item in setitems if item] 
         return (testdata, searchdata)
 
+    def get_allowed_items(self,projectKey):
+        items = [item.strip() for item in self.allowed_values.split(",") if item.strip()]
+        setitems = sorted(list(set(items)))
+        testdata = [{"label" : item.strip(), "value": item.strip()} for item in setitems if item]
+        return testdata
+
     def get_space_replaced_name(self):
         return self.name.replace(u" ", u"__space__")
 
