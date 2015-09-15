a3ec244595ab9126f008d16220aeb3b8a711873d
Strets123 <strets123@gmail.com>
2015-09-15 19:23:51 +0100

    new migrations

---
+++ cbh_core_model/migrations/0018_auto_20150915_1238.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+def create_data_types(apps, schema_editor):
+    
+    DataType = apps.get_model("cbh_core_model", "DataType")
+    DataType.objects.get_or_create(name="Project")
+    DataType.objects.get_or_create(name="Sub-Project")
+    DataType.objects.get_or_create(name="Assay")
+    DataType.objects.get_or_create(name="Activity")
+    DataType.objects.get_or_create(name="Study")
+    ProjectType = apps.get_model("cbh_core_model", "ProjectType")
+    ProjectType.objects.get_or_create(name="chemical")
+    ProjectType.objects.get_or_create(name="assay")
+    ProjectType.objects.get_or_create(name="inventory")
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('cbh_core_model', '0017_auto_20150915_0022'),
+    ]
+
+
+    operations = [
+        migrations.RunPython(create_data_types),
+    ]
+
