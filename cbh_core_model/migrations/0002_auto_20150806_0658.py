bcb3cedaaa3307802734a87e2cbd75111f3cdfd4
Strets123 <strets123@gmail.com>
2016-05-02 19:39:03 +0100

    migration tweaks foir django 1.9

--- cbh_core_model/migrations/0002_auto_20150806_0658.py
+++ cbh_core_model/migrations/0002_auto_20150806_0658.py
@@ -8,6 +8,8 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('cbh_core_model', '0001_initial'),
+        ('cbh_chembl_model_extension', '0025_auto_20150807_0933'),
+
     ]
 
     operations = [
--- cbh_core_model/migrations/0023_auto_20151105_1850.py
+++ cbh_core_model/migrations/0023_auto_20151105_1850.py
@@ -7,10 +7,10 @@ def migrate_perms(apps, schema_editor):
     ContentType = apps.get_model("contenttypes", "contenttype")
     Permission = apps.get_model("auth", "Permission")
     ct, created = ContentType.objects.get_or_create(
-            app_label="_can_see", model=ContentType, name="Apps which the user can see")
+            app_label="_can_see", model=ContentType)
       
-    pm = Permission.objects.get_or_create( content_type_id=ct.id, codename="no_chemreg", name="ChemiReg Disabled")
-    pm = Permission.objects.get_or_create( content_type_id=ct.id, codename="no_assayreg", name="AssayReg Disabled")
+    pm = Permission.objects.get_or_create( content_type_id=ct.id, codename="no_chemreg")
+    pm = Permission.objects.get_or_create( content_type_id=ct.id, codename="no_assayreg")
 
 
 class Migration(migrations.Migration):
